2a384b5fea3df7f800395ba5c8800c20ac3897d5
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2023-02-07 02:00:41 +0100

    Add styling to desktop version

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -4,6 +4,7 @@ interface Dresses {
   name: string,
   author: string,
   image: string,
+  amount: number,
 }
 
 @Component({
@@ -17,90 +18,105 @@ export class BooksComponent implements OnInit {
       name: 'skirt and blouse',
       author: 'Gucci',
       image: 'assets/images/yellow-dress.jpg',
+      amount: 15000
     },
 
     {
       image: 'assets/images/black.jpg',
       name: 'Black dress',
-      author: 'Versace'
+      author: 'Versace',
+      amount: 20000
     },
 
     {
       image: 'assets/images/suitwhite.jpg',
       name: 'white suit',
       author: 'Givenchi',
+      amount: 25000
     },
 
     {
       image: 'assets/images/suit.jpg',
       name: 'Dark-red suit',
       author: 'Fenty',
+      amount: 30000,
     },
 
     {
       image: 'assets/images/blue-dress.jpg',
       name: 'Blue dress',
       author: 'Louise-vuitton',
+      amount: 35000
     },
 
     {
       image: 'assets/images/blue.jpg',
       name: 'Nude color dress',
-      author: 'Normain',    
+      author: 'Normain',
+      amount: 40000
     },
     
     {
       image: 'assets/images/rose.jpg',
       name: 'Nude color dress',
-      author: 'Dolce & Gabana',    
+      author: 'Dolce & Gabana',
+      amount: 45000
     },
 
     {
       image: 'assets/images/green.jpg',
       name: 'green dress',
-      author: 'Fenty',    
+      author: 'Fenty',
+      amount: 50000
     },
 
     {
       image: 'assets/images/glitter.jpg',
       name: 'gliter dress',
-      author: 'Dior',    
+      author: 'Dior',
+      amount: 30000,
     },
 
     {
       image: 'assets/images/orange.jpg',
       name: 'Orange dress',
-      author: 'Versace',    
+      author: 'Versace',
+      amount: 45000
     },
 
     {
       image: 'assets/images/white.jpg',
       name: 'White dress',
-      author: 'Hermes',    
+      author: 'Hermes',
+      amount: 35000
     },
 
     {
       image: 'assets/images/brown.jpg',
-      name: 'Browndress',
-      author: 'Normain',    
+      name: 'Brown dress',
+      author: 'Normain',
+      amount: 50000
     },
 
     {
       image: 'assets/images/yellow.jpg',
       name: 'Yellow dress',
-      author: 'Givenchi',    
+      author: 'Givenchi',
+      amount: 25000
     },
 
     {
       image: 'assets/images/brown-dress.jpg',
       name: 'Brown dress',
-      author: 'Dior',    
+      author: 'Dior',
+      amount: 50000
     },
 
     {
       image: 'assets/images/pink.jpg',
       name: 'Pink dress',
-      author: 'versace',    
+      author: 'versace',
+      amount: 25000
     },
   ]
 
@@ -109,6 +125,10 @@ export class BooksComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  addToCard() {
+    console.log(this.dress)
+  }
 
   
 
