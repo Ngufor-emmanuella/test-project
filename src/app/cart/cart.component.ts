cd29fa99e4f21bbb4605655f05a257e5582b5946
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2023-02-10 04:27:18 +0100

    Add queries

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from './cart.service';
+import { Books } from '../interface/Book';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+
+  constructor(private bookService: CartService) {}
+  cart: Books[] = [];
+
+  isShowing: boolean = true;
+
+  ngOnInit(): void {
+    this.cart = this.bookService.get();
+  }
+
 
 }
