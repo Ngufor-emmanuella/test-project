51364dd51ba0e9f262bb77507182ce69362ec61c
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2023-02-21 15:41:05 +0100

    Add background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,20 +7,5 @@ import { Component } from "@angular/core";
 })
 
 export class AppComponent {
-  // isFixedNavbar;
-  // @HostBinding('class.navbar-opened') navbarOpened = false;
-  // constructor(
-  // ) { }
-  // ngOnInit() {
-  //   const offset = window.pageYoffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-  //   if(offset > 10) {
-  //     this.isFixedNavbar = true;
-  //   } else {
-  //     this.isFixedNavbar = false;
-  //   }
-  // }
+  
 }
-
-// toggleNavbar() {
-//   this.navbarOpened = !this.navbarOpened;
-// }
